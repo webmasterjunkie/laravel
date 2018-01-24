32736ec24099dc9fbd238e2eca43f5a1982f0810
Cris Griffith <cristopher.griffith@gmail.com>
2018-01-24 13:27:29 -0500

    Add MySQL Key Length Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Fix key length error for < MySQL 5.7.7
+        Schema::defaultStringLength(191);
     }
 
     /**
